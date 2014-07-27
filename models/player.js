21a9470bc68219f579e0ec14c8ad5ecb283134f7
Bo <hizhubo@gmail.com>
2014-07-27 10:00:22 +0800

    Added players ejs template and dummy data.

---
+++ models/player.js
@@ -0,0 +1,108 @@
+/* Object/Relational mapping for instances of the Order class.
+- classes correspond to tables
+- instances correspond to rows
+- fields correspond to columns
+In other words, this code defines how a row in the postgres Player table
+maps to the JS Order object.
+ */
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define("Player", {
+    id : {
+      type : DataTypes.INTEGER,
+      unique : true,
+      allowNull : false,
+      autoIncrement : true
+    },
+    firstName : {
+      type : DataTypes.STRING,
+      allowNull : false
+    },
+    lastName : {
+      type : DataTypes.STRING,
+      allowNull : false
+    },
+    email : {
+      type : DataTypes.STRING,
+      allowNull : false,
+      validate : {
+        isEmail : true,
+      }
+    },
+    password : {
+      type : DataTypes.STRING
+    },
+    mobile : {
+      type : DataTypes.STRING
+    },
+    wechat : {
+      type : DataTypes.STRING
+    },
+    favoritePosition : {
+      type : DataTypes.STRING
+    },
+    latestEndurance : {
+      type : DataTypes.INTEGER,
+      allowNull : false,
+      validate : {
+        min : 1,
+        max : 5
+      }
+    },
+    latestSpeed : {
+      type : DataTypes.INTEGER,
+      allowNull : false,
+      validate : {
+        min : 1,
+        max : 5
+      }
+    },
+    latestDribble : {
+      type : DataTypes.INTEGER,
+      allowNull : false,
+      validate : {
+        min : 1,
+        max : 5
+      }
+    },
+    latestPass : {
+      type : DataTypes.INTEGER,
+      allowNull : false,
+      validate : {
+        min : 1,
+        max : 5
+      }
+    },
+    latestDefense : {
+      type : DataTypes.INTEGER,
+      allowNull : false,
+      validate : {
+        min : 1,
+        max : 5
+      }
+    },
+    latestShoot : {
+      type : DataTypes.INTEGER,
+      allowNull : false,
+      validate : {
+        min : 1,
+        max : 5
+      }
+    },
+    latestStrength : {
+      type : DataTypes.INTEGER,
+      allowNull : false,
+      validate : {
+        min : 1,
+        max : 5
+      }
+    },
+    latestOverallAbility : {
+      type : DataTypes.INTEGER,
+      allowNull : false,
+      validate : {
+        min : 0,
+        max : 100
+      }
+    }
+  });
+};
--- web.js
+++ web.js
@@ -1,12 +1,27 @@
 // web.js
 var express = require("express");
-var logfmt = require("logfmt");
 var app = express();
-
-app.use(logfmt.requestLogger());
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.send('The start of Football Club Manager!');
+  // res.send('The start of Football Club Manager!');
+  res.render("players", {
+    players : [{
+        "id" : 1,
+        "firstName" : "Bo",
+        "lastName" : "Zhu",
+        "latestEndurance" : 3,
+        "latestSpeed" : 4,
+        "latestDribble" : 4,
+        "latestPass" : 4,
+        "latestDefense" : 5,
+        "latestShoot" : 5,
+        "latestStrength" : 3,
+        "latestOverallAbility" : 90
+      }
+    ]
+  });
 });
 
 var port = Number(process.env.PORT || 81);
